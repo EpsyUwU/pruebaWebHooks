53626118f05716c5b2a3e25a4199869b3eee32af
Esteban <98122124+EpsyUwU@users.noreply.github.com>
2023-03-15 21:50:42 -0600

    colita china

---
+++ webhook.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+
+const handler = createHandler({ path: '/webhook', secret: 'mysecret' });
+console.log("ya ta jalando en el puerto 3000")
+
+http.createServer((req, res) => {
+    
+  handler(req, res, err => {
+    res.statusCode = 404;
+    res.end('404 - Not Found');
+  });
+}).listen(3000);
+
+handler.on('error', err => console.error('Error:', err.message));
+
+handler.on('push', event => {
+  console.log('Push recibido en el repositorio:', event.payload.repository.name);
+});
