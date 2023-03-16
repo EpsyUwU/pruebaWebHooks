195e523a9674cd5b825822b1d0afab5c5494cc01
Esteban <98122124+EpsyUwU@users.noreply.github.com>
2023-03-15 22:07:54 -0600

    probando el code

--- webhook.js
+++ webhook.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const createHandler = require('github-webhook-handler');
 
-const handler = createHandler({ path: '/webhook', secret: 'mysecret' });
+const handler = createHandler({ path: '/webhook', secret: 'esteban' });
 console.log("ya ta jalando en el puerto 3000")
 
 http.createServer((req, res) => {
-    
   handler(req, res, err => {
     res.statusCode = 404;
     res.end('404 - Not Found');
